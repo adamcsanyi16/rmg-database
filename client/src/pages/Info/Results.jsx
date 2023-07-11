0efd43fa8d5564d44311de90440be710f6823a42
adamcsanyi16 <csanyiadam8@gmail.com>
2023-07-11 12:49:47 +0200

    filters-ui

--- client/src/pages/Info/Results.jsx
+++ client/src/pages/Info/Results.jsx
@@ -1,8 +1,7 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import { Link } from "react-router-dom";
 import { useAuthContext } from "../../hooks/useAuthContext";
 import Modal from "react-modal";
-//import Select from "react-select";
 
 const Results = () => {
   //VARIABLES
@@ -17,7 +16,6 @@ const Results = () => {
   const { user } = useAuthContext();
   const [isAdmin, setIsAdmin] = useState(false);
   const [backToTop, setBackToTop] = useState(false);
-  const [checkedBoxesVtipus, setCheckedBoxesVtipus] = useState([]);
 
   //FETCHING ISADMIN
   useEffect(() => {
@@ -77,6 +75,8 @@ const Results = () => {
     data();
   }, []);
 
+  //CHECKBOXES
+
   //SEARCHING
   useEffect(() => {
     const filtered = results.filter((result) => {
@@ -115,23 +115,28 @@ const Results = () => {
   };
 
   //BACKTOTOP USEEFFECT
+  const tableContainerRef = useRef(null);
+
   useEffect(() => {
-    window.addEventListener("scroll", handleScroll);
+    const tableContainer = tableContainerRef.current;
+
+    const handleScroll = () => {
+      if (tableContainer.scrollTop > 100) {
+        setBackToTop(true);
+      } else {
+        setBackToTop(false);
+      }
+    };
+
+    tableContainer.addEventListener("scroll", handleScroll);
     return () => {
-      window.removeEventListener("scroll", handleScroll);
+      tableContainer.removeEventListener("scroll", handleScroll);
     };
   }, []);
 
-  const handleScroll = () => {
-    if (window.scrollY > 100) {
-      setBackToTop(true);
-    } else {
-      setBackToTop(false);
-    }
-  };
-
   const scrollToTop = () => {
-    window.scrollTo({ top: 0, behavior: "smooth" });
+    const tableContainer = tableContainerRef.current;
+    tableContainer.scrollTo({ top: 0, behavior: "smooth" });
   };
 
   //PAGES OF RESULTS
@@ -214,163 +219,178 @@ const Results = () => {
       margin: "auto",
       padding: "20px",
       backgroundColor: "fff",
+      borderRadius: "2rem",
     },
   };
 
   return (
-    <div>
-      <div className="sorting-container">
-        {/*<label class="container">
-          <input
-            type="checkbox"
-            checked={checkedBoxesVtipus.includes("tanulmányi")}
-            onChange={() => handleCheckboxChange("tanulmányi")}
-          />
-          <div class="checkmark"></div>
-          <p>Tanulmányi</p>
-        </label>
-        <label class="container">
-          <input
-            type="checkbox"
-            checked={checkedBoxesVtipus.includes("sport")}
-            onChange={() => handleCheckboxChange("sport")}
-          />
-          <div class="checkmark"></div>
-          <p>Sport</p>
-        </label>
-        <label class="container">
-          <input
-            type="checkbox"
-            checked={checkedBoxesVtipus.includes("művészeti")}
-            onChange={() => handleCheckboxChange("művészeti")}
-          />
-          <div class="checkmark"></div>
-          <p>Művészeti</p>
-        </label>
-        <label class="container">
-          <input
-            type="checkbox"
-            checked={checkedBoxesVtipus.includes("országos")}
-            onChange={() => handleCheckboxChange("országos")}
-          />
-          <div class="checkmark"></div>
-          <p>Országos</p>
-        </label>*/}
-        <div className="filter">
-          <input
-            type="text"
-            placeholder="Keresés..."
-            value={searchTerm}
-            onChange={handleSearchChange}
-          />
-        </div>
-      </div>
-      <div className="table-container">
-        {success && <div className="success">{success}</div>}
-        {error && <div className="error">{error}</div>}
-        <div className="allinfo">
-          <div className="table">
-            <table className="styled-table">
-              <thead>
-                <tr>
-                  {isAdmin && <th>Felvevő email</th>}
-                  <th>Verseny típusa</th>
-                  <th>Verseny szintje</th>
-                  <th>Verseny neve</th>
-                  <th>Ágazat</th>
-                  <th>Verseny formája</th>
-                  <th>Helyezés</th>
-                  <th>Tanulók</th>
-                  <th>Osztály</th>
-                  <th>Felkészítő tanár(ok)</th>
-                  {isAdmin && <th></th>}
-                  {isAdmin && <th></th>}
-                </tr>
-              </thead>
-              <tbody>
-                {currentResults.map((result) => (
-                  <tr key={result._id}>
-                    {isAdmin && <td>{result.nev}</td>}
-                    <td>{result.vtipus}</td>
-                    <td>{result.vszint}</td>
-                    <td>{result.verseny}</td>
-                    <td>{result.agazat}</td>
-                    <td>{result.vforma}</td>
-                    <td>{result.helyezes}</td>
-                    <td>{result.tanulok}</td>
-                    <td>{result.osztaly}</td>
-                    <td>{result.tanarok}</td>
-                    {isAdmin && (
-                      <td>
-                        <Link to={"/eredmenyek/" + result._id}>
-                          <button className="btn">
-                            <svg
-                              className="icon"
-                              strokeLinejoin="round"
-                              strokeLinecap="round"
-                              fill="none"
-                              height="24"
-                              width="24"
-                              viewBox="0 0 24 24"
-                            >
-                              <path d="M17 3a2.828 2.828 0 1 1 4 4L7.5 20.5 2 22l1.5-5.5L17 3z"></path>
-                            </svg>
-                          </button>
-                        </Link>
-                      </td>
-                    )}
-                    {isAdmin && (
-                      <td>
-                        <button
-                          className="btn"
-                          onClick={() => deleteId(result._id)}
-                        >
-                          <svg
-                            viewBox="0 0 15 17.5"
-                            height="24"
-                            width="24"
-                            xmlns="http://www.w3.org/2000/svg"
-                            className="icon"
-                          >
-                            <path
-                              transform="translate(-2.5 -1.25)"
-                              d="M15,18.75H5A1.251,1.251,0,0,1,3.75,17.5V5H2.5V3.75h15V5H16.25V17.5A1.251,1.251,0,0,1,15,18.75ZM5,5V17.5H15V5Zm7.5,10H11.25V7.5H12.5V15ZM8.75,15H7.5V7.5H8.75V15ZM12.5,2.5h-5V1.25h5V2.5Z"
-                              id="Fill"
-                            ></path>
-                          </svg>
-                        </button>
-                      </td>
-                    )}
-                  </tr>
-                ))}
-              </tbody>
-            </table>
-            <p>Oldal: {currentPage}</p>
-            <div className="toggle-buttons">
-              {pageNumbers.map((pageNumber) => (
-                <button
-                  key={pageNumber}
-                  onClick={() => setCurrentPage(pageNumber)}
-                  disabled={pageNumber === currentPage}
-                >
-                  {pageNumber}
-                </button>
-              ))}
+    <div className="results">
+      <div className="results-wrap">
+        <div className="sorting-container">
+          <div className="filter">
+            <input
+              type="text"
+              placeholder="Keresés..."
+              value={searchTerm}
+              onChange={handleSearchChange}
+            />
+          </div>
+          <div className="vtipus">
+            <span id="category">Versenytípus</span>
+            <div className="checkbox">
+              <label className="container">
+                <input type="checkbox" />
+                <div className="checkmark"></div>
+              </label>
+              <span>Tanulmányi</span>
+            </div>
+            <div className="checkbox">
+              <label className="container">
+                <input type="checkbox" />
+                <div className="checkmark"></div>
+              </label>
+              <span>Sport</span>
+            </div>
+            <div className="checkbox">
+              <label className="container">
+                <input type="checkbox" />
+                <div className="checkmark"></div>
+              </label>
+              <span>Művészeti</span>
+            </div>
+          </div>
+          <div className="vszint">
+            <span id="category">Versenyszint</span>
+            <div className="checkbox">
+              <label className="container">
+                <input type="checkbox" />
+                <div className="checkmark"></div>
+              </label>
+              <span>Regionális/területi</span>
+            </div>
+            <div className="checkbox">
+              <label className="container">
+                <input type="checkbox" />
+                <div className="checkmark"></div>
+              </label>
+              <span>Országos</span>
+            </div>
+            <div className="checkbox">
+              <label className="container">
+                <input type="checkbox" />
+                <div className="checkmark"></div>
+              </label>
+              <span>Nemzetközi</span>
             </div>
           </div>
         </div>
-        <Modal
-          isOpen={showModal}
-          onRequestClose={closeModal}
-          contentLabel="Megerősítés"
-          style={modalStyles}
-        >
-          <h2>Biztos hogy törlöd az adatot?</h2>
-          <div className="modal-buttons">
-            <button onClick={() => torol(itemToDelete)}>Törlés</button>
-            <button onClick={closeModal}>Mégsem</button>
+        <div className="table-container" ref={tableContainerRef}>
+          {success && <div className="success">{success}</div>}
+          {error && <div className="error">{error}</div>}
+          <div className="allinfo">
+            <div className="table">
+              <table className="styled-table">
+                <thead>
+                  <tr>
+                    {isAdmin && <th>Felvevő email</th>}
+                    <th>Verseny típusa</th>
+                    <th>Verseny szintje</th>
+                    <th>Verseny neve</th>
+                    <th>Ágazat</th>
+                    <th>Verseny formája</th>
+                    <th>Helyezés</th>
+                    <th>Tanulók</th>
+                    <th>Osztály</th>
+                    <th>Felkészítő tanár(ok)</th>
+                    {isAdmin && <th></th>}
+                    {isAdmin && <th></th>}
+                  </tr>
+                </thead>
+                <tbody>
+                  {currentResults.map((result) => (
+                    <tr key={result._id}>
+                      {isAdmin && <td>{result.nev}</td>}
+                      <td>{result.vtipus}</td>
+                      <td>{result.vszint}</td>
+                      <td>{result.verseny}</td>
+                      <td>{result.agazat}</td>
+                      <td>{result.vforma}</td>
+                      <td>{result.helyezes}</td>
+                      <td>{result.tanulok}</td>
+                      <td>{result.osztaly}</td>
+                      <td>{result.tanarok}</td>
+                      {isAdmin && (
+                        <td>
+                          <Link to={"/eredmenyek/" + result._id}>
+                            <button className="btn">
+                              <svg
+                                className="icon"
+                                strokeLinejoin="round"
+                                strokeLinecap="round"
+                                fill="none"
+                                height="24"
+                                width="24"
+                                viewBox="0 0 24 24"
+                              >
+                                <path d="M17 3a2.828 2.828 0 1 1 4 4L7.5 20.5 2 22l1.5-5.5L17 3z"></path>
+                              </svg>
+                            </button>
+                          </Link>
+                        </td>
+                      )}
+                      {isAdmin && (
+                        <td>
+                          <button
+                            className="btn"
+                            onClick={() => deleteId(result._id)}
+                          >
+                            <svg
+                              viewBox="0 0 15 17.5"
+                              height="24"
+                              width="24"
+                              xmlns="http://www.w3.org/2000/svg"
+                              className="icon"
+                            >
+                              <path
+                                transform="translate(-2.5 -1.25)"
+                                d="M15,18.75H5A1.251,1.251,0,0,1,3.75,17.5V5H2.5V3.75h15V5H16.25V17.5A1.251,1.251,0,0,1,15,18.75ZM5,5V17.5H15V5Zm7.5,10H11.25V7.5H12.5V15ZM8.75,15H7.5V7.5H8.75V15ZM12.5,2.5h-5V1.25h5V2.5Z"
+                                id="Fill"
+                              ></path>
+                            </svg>
+                          </button>
+                        </td>
+                      )}
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+              <p>Oldal: {currentPage}</p>
+              <div className="toggle-buttons">
+                {pageNumbers.map((pageNumber) => (
+                  <button
+                    key={pageNumber}
+                    onClick={() => setCurrentPage(pageNumber)}
+                    disabled={pageNumber === currentPage}
+                  >
+                    {pageNumber}
+                  </button>
+                ))}
+              </div>
+            </div>
           </div>
-        </Modal>
+          <Modal
+            isOpen={showModal}
+            onRequestClose={closeModal}
+            contentLabel="Megerősítés"
+            style={modalStyles}
+          >
+            <h2>Biztos hogy törlöd az adatot?</h2>
+            <div className="modal-buttons">
+              <button onClick={() => torol(itemToDelete)}>Törlés</button>
+              <button onClick={closeModal}>Mégsem</button>
+            </div>
+          </Modal>
+        </div>
       </div>
       <button
         className={`back-to-top ${backToTop ? "show" : ""}`}
